02e760fa10b73d68d4c72474c305d5da4f5614aa
susanocontact11-sys <susanocontact11@gmail.com>
2026-09-20 18:15:44 +0100

    Simplify next.config.js to an empty config
    Removed custom webpack configuration for non-server builds.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      const externals = Array.isArray(config.externals) ? config.externals : [];
-      config.externals = [
-        ...externals,
-        {
-          '@capacitor-community/background-geolocation':
-            'commonjs @capacitor-community/background-geolocation',
-        },
-      ];
-    }
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
